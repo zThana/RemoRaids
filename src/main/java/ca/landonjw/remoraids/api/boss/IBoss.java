9b86fb41430b12caa799c101728122441be1d43b
Nick DeGruccio <nick.degruccio@gmail.com>
2020-06-02 10:50:21 -0700

    Ensure we don't mix spaces and tabs, default to tabs

--- src/main/java/ca/landonjw/remoraids/api/boss/IBoss.java
+++ src/main/java/ca/landonjw/remoraids/api/boss/IBoss.java
@@ -19,7 +19,7 @@ import java.util.Optional;
 /**
  * A IBoss represents the raid boss that will be used as a raid party host. In other words,
  * this is the raid pokemon that players will fight together in order to take it down.
- *
+ * <p>
  * A boss can be customized as much as a user desires, and external third party options
  * are free to use a raid boss's data for any necessary means they desire.
  */
@@ -31,7 +31,7 @@ public interface IBoss extends DataSerializable {
 	 *
 	 * @return The pokemon representing this raid boss
 	 */
-    Pokemon getPokemon();
+	Pokemon getPokemon();
 
 	/**
 	 * Specifies the current total of a stat. This is more of a convenience method, as the representative pokemon
@@ -46,17 +46,17 @@ public interface IBoss extends DataSerializable {
 	 * Applies the input value to the specified stat type. This is a direct setter, and makes no mathematical
 	 * adjustments.
 	 *
-	 * @param stat The stat type to apply the input value to
+	 * @param stat  The stat type to apply the input value to
 	 * @param value The value to use to represent the stat
 	 */
-    void setStat(StatsType stat, int value);
+	void setStat(StatsType stat, int value);
 
 	/**
 	 * Amplifies the calculated stat of the pokemon by the multiplier specified in the input.
 	 * In other words, this takes the current value of the stat type of the raid boss, and multiplies
 	 * it by the amplifier value.
 	 *
-	 * @param stat The stat type to apply the input value to
+	 * @param stat      The stat type to apply the input value to
 	 * @param amplifier The value to multiply the stat by
 	 */
 	void amplifyStat(StatsType stat, double amplifier);
@@ -67,7 +67,7 @@ public interface IBoss extends DataSerializable {
 	 *
 	 * @return The value representing the amplification to the raid boss's size
 	 */
-    float getSize();
+	float getSize();
 
 	/**
 	 * Applies a new scale to the raid boss's statue. Use care when setting a value, as too big a value
@@ -83,7 +83,7 @@ public interface IBoss extends DataSerializable {
 	 *
 	 * @return The texture of the pokemon, if it has a custom texture
 	 */
-    Optional<String> getTexture();
+	Optional<String> getTexture();
 
 	/**
 	 * Applies a custom texture to the raid boss. Assuming a texture exists by its name, this texture
@@ -99,144 +99,144 @@ public interface IBoss extends DataSerializable {
 	 *
 	 * @return A new builder allowing construction of a raid boss
 	 */
-    static IBossBuilder builder() {
-        return IBossAPI.getInstance().getRaidRegistry().createBuilder(IBossBuilder.class);
-    }
+	static IBossBuilder builder() {
+		return IBossAPI.getInstance().getRaidRegistry().createBuilder(IBossBuilder.class);
+	}
 
-    /**
-     * Represents the building method for creating a raid boss. Out of all the available options listed below, the
-     * only required field should be the species of the raid boss. Failure to specify this value will result in
-     * a generated exception, terminating the build.
-     */
-    interface IBossBuilder extends IBuilder<IBoss, IBossBuilder> {
+	/**
+	 * Represents the building method for creating a raid boss. Out of all the available options listed below, the
+	 * only required field should be the species of the raid boss. Failure to specify this value will result in
+	 * a generated exception, terminating the build.
+	 */
+	interface IBossBuilder extends IBuilder<IBoss, IBossBuilder> {
 
-        /**
-         * Builds the raid boss from a {@link PokemonSpec}. Essentially, for all build options
-         * available, this function will call each and every one of them.
-         *
-         * @param spec The PokemonSpec that'll be used to create the raid boss.
-         * @return The builder after being modified by this call
-         */
-        IBossBuilder spec(PokemonSpec spec);
+		/**
+		 * Builds the raid boss from a {@link PokemonSpec}. Essentially, for all build options
+		 * available, this function will call each and every one of them.
+		 *
+		 * @param spec The PokemonSpec that'll be used to create the raid boss.
+		 * @return The builder after being modified by this call
+		 */
+		IBossBuilder spec(PokemonSpec spec);
 
-        /**
-         * Sets the species of the raid boss to the input species. If a spec is not defined, or one
-         * that is doesn't specify a species, this call will end up being required. If no species for
-         * the boss is ever specified, the build operations will fail exceptionally.
-         *
-         * @param species The species of the pokemon to set as the raid boss
-         * @return The builder after being modified by this call
-         */
-        IBossBuilder species(EnumSpecies species);
+		/**
+		 * Sets the species of the raid boss to the input species. If a spec is not defined, or one
+		 * that is doesn't specify a species, this call will end up being required. If no species for
+		 * the boss is ever specified, the build operations will fail exceptionally.
+		 *
+		 * @param species The species of the pokemon to set as the raid boss
+		 * @return The builder after being modified by this call
+		 */
+		IBossBuilder species(EnumSpecies species);
 
-        /**
-         * Sets the level of the raid boss. This will often times be used for just display, but
-         * depending on reward definitions and lack of a specified moveset, this will be used for generating
-         * stats and the like.
-         *
-         * @param level The level of the raid boss
-         * @return The builder after being modified by this call
-         */
-        IBossBuilder level(int level);
+		/**
+		 * Sets the level of the raid boss. This will often times be used for just display, but
+		 * depending on reward definitions and lack of a specified moveset, this will be used for generating
+		 * stats and the like.
+		 *
+		 * @param level The level of the raid boss
+		 * @return The builder after being modified by this call
+		 */
+		IBossBuilder level(int level);
 
-        /**
-         * Sets the form of the raid boss. For instance, this can be used to create a raid boss that is a
-         * primal groudon, or a Sir Doofus III.
-         *
-         * @param form The form to have the raid boss model
-         * @return The builder after being modified by this call
-         */
-        IBossBuilder form(IEnumForm form);
+		/**
+		 * Sets the form of the raid boss. For instance, this can be used to create a raid boss that is a
+		 * primal groudon, or a Sir Doofus III.
+		 *
+		 * @param form The form to have the raid boss model
+		 * @return The builder after being modified by this call
+		 */
+		IBossBuilder form(IEnumForm form);
 
-        /**
-         * Sets whether or not the raid boss will be shiny.
-         *
-         * @param shiny The shiny state of the raid boss
-         * @return The builder after being modified by this call
-         */
-        IBossBuilder shiny(boolean shiny);
+		/**
+		 * Sets whether or not the raid boss will be shiny.
+		 *
+		 * @param shiny The shiny state of the raid boss
+		 * @return The builder after being modified by this call
+		 */
+		IBossBuilder shiny(boolean shiny);
 
-        /**
-         * Sets the nature of the raid boss.
-         *
-         * @param nature The nature of the raid boss
-         * @return The builder after being modified by this call
-         */
-        IBossBuilder nature(EnumNature nature);
+		/**
+		 * Sets the nature of the raid boss.
+		 *
+		 * @param nature The nature of the raid boss
+		 * @return The builder after being modified by this call
+		 */
+		IBossBuilder nature(EnumNature nature);
 
-        /**
-         * Sets the ability of the raid boss. This call will allow for abilities the pokemon may not normally
-         * be able to come with.
-         *
-         * @param ability The ability to apply to the raid boss
-         * @return The builder after being modified by this call
-         */
-        IBossBuilder ability(String ability);
+		/**
+		 * Sets the ability of the raid boss. This call will allow for abilities the pokemon may not normally
+		 * be able to come with.
+		 *
+		 * @param ability The ability to apply to the raid boss
+		 * @return The builder after being modified by this call
+		 */
+		IBossBuilder ability(String ability);
 
-	    /**
-	     * Sets the gender of the raid boss. The gender should be a valid option. Invalid options
-	     * will be ignored, and the gender will be set randomly as always.
-	     *
-	     * @param gender The gender to apply to the raid boss
-	     * @return The builder after being modified by this call
-	     */
-	    IBossBuilder gender(Gender gender);
+		/**
+		 * Sets the gender of the raid boss. The gender should be a valid option. Invalid options
+		 * will be ignored, and the gender will be set randomly as always.
+		 *
+		 * @param gender The gender to apply to the raid boss
+		 * @return The builder after being modified by this call
+		 */
+		IBossBuilder gender(Gender gender);
 
-        /**
-         * Applies a stat modification to the raid boss for a specific stat. More often than not, you'll only
-         * want to apply this for health. While you can apply stat modifications elsewhere, it may make the opposing
-         * pokemon far too powerful to defeat. Use with caution!
-         *
-         * <p>You may also use this call to apply an amplifier. An amplifier essentially takes the input value
-         * for the stat, and treats it as a multiplier to the overall stat of the pokemon, unmodified. Stat modifications
-         * do not stack, so a change to a stat overwrites any previous application.</p>
-         *
-         * @param stat The stat to apply the modification to
-         * @param input The value to use when modifying the stat
-         * @param amplify Whether or not the input value should be a multiplier or static value.
-         * @return The builder after being modified by this call
-         */
-        IBossBuilder stat(StatsType stat, int input, boolean amplify);
+		/**
+		 * Applies a stat modification to the raid boss for a specific stat. More often than not, you'll only
+		 * want to apply this for health. While you can apply stat modifications elsewhere, it may make the opposing
+		 * pokemon far too powerful to defeat. Use with caution!
+		 *
+		 * <p>You may also use this call to apply an amplifier. An amplifier essentially takes the input value
+		 * for the stat, and treats it as a multiplier to the overall stat of the pokemon, unmodified. Stat modifications
+		 * do not stack, so a change to a stat overwrites any previous application.</p>
+		 *
+		 * @param stat    The stat to apply the modification to
+		 * @param input   The value to use when modifying the stat
+		 * @param amplify Whether or not the input value should be a multiplier or static value.
+		 * @return The builder after being modified by this call
+		 */
+		IBossBuilder stat(StatsType stat, int input, boolean amplify);
 
-        /**
-         * Sets the overall size of the raid boss. This will be the scale of the model as it will appear to the
-         * client. Be generous on this value, as some pokemon with high values here can be quite massive!
-         * Use with care!
-         *
-         * @param size The size factor of the raid boss
-         * @return The builder after being modified by this call
-         */
-        IBossBuilder size(float size);
+		/**
+		 * Sets the overall size of the raid boss. This will be the scale of the model as it will appear to the
+		 * client. Be generous on this value, as some pokemon with high values here can be quite massive!
+		 * Use with care!
+		 *
+		 * @param size The size factor of the raid boss
+		 * @return The builder after being modified by this call
+		 */
+		IBossBuilder size(float size);
 
-        /**
-         * Allows for applying a custom texture to the raid pokemon.
-         *
-         * <p>If you aim to have no texture for this pokemon, use "". If null is placed here,
-         * the call will fail exceptionally. Blame pixelmon for this design decision.</p>
-         *
-         * @param texture The texture to use for the raid boss.
-         * @return The builder after being modified by this call
-         */
-        IBossBuilder texture(@NonNull String texture);
+		/**
+		 * Allows for applying a custom texture to the raid pokemon.
+		 *
+		 * <p>If you aim to have no texture for this pokemon, use "". If null is placed here,
+		 * the call will fail exceptionally. Blame pixelmon for this design decision.</p>
+		 *
+		 * @param texture The texture to use for the raid boss.
+		 * @return The builder after being modified by this call
+		 */
+		IBossBuilder texture(@NonNull String texture);
 
-        /**
-         * Applies the given moveset to the raid boss. If the boss is created with a restricted move, the system
-         * will send a warn message to indicate the issue, and then subsequently ignore that particular move. Fails
-         * quietly if no moves are available or all were rejected.
-         *
-         * @param moveset The moveset of the raid boss
-         * @return The builder after being modified by this call
-         */
-        IBossBuilder moveset(Moveset moveset);
+		/**
+		 * Applies the given moveset to the raid boss. If the boss is created with a restricted move, the system
+		 * will send a warn message to indicate the issue, and then subsequently ignore that particular move. Fails
+		 * quietly if no moves are available or all were rejected.
+		 *
+		 * @param moveset The moveset of the raid boss
+		 * @return The builder after being modified by this call
+		 */
+		IBossBuilder moveset(Moveset moveset);
 
-	    /**
-	     * Deserializes a set of JSON data to populate any set fields within the json data.
-	     *
-	     * @param json The JSON data to deserialize
-	     * @return The builder after being modified by this call
-	     */
-	    IBossBuilder fromJson(JObject json);
+		/**
+		 * Deserializes a set of JSON data to populate any set fields within the json data.
+		 *
+		 * @param json The JSON data to deserialize
+		 * @return The builder after being modified by this call
+		 */
+		IBossBuilder fromJson(JObject json);
 
-    }
+	}
 
 }
