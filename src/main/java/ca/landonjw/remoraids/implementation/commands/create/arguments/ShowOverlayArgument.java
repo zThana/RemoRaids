f449f13f74db920b5201f3e6ffab8339c8371e2a
Qumoo <76853697+Qumoo@users.noreply.github.com>
2021-07-04 18:03:05 +0200

    Added argument to command

---
+++ src/main/java/ca/landonjw/remoraids/implementation/commands/create/arguments/ShowOverlayArgument.java
@@ -0,0 +1,30 @@
+package ca.landonjw.remoraids.implementation.commands.create.arguments;
+
+import ca.landonjw.remoraids.RemoRaids;
+import ca.landonjw.remoraids.api.commands.arguments.IRaidsArgument;
+import ca.landonjw.remoraids.api.messages.placeholders.IParsingContext;
+import ca.landonjw.remoraids.api.spawning.IBossSpawner;
+import ca.landonjw.remoraids.internal.config.MessageConfig;
+import com.google.common.collect.Lists;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+
+public class ShowOverlayArgument implements IRaidsArgument {
+
+    @Override
+    public List<String> getTokens() {
+        return Lists.newArrayList("show-overlay", "sh-ov");
+    }
+
+    @Override
+    public void interpret(@Nonnull String value, @Nonnull IParsingContext context) throws IllegalArgumentException {
+        if (context.getAssociation(IBossSpawner.IBossSpawnerBuilder.class).isPresent()) {
+            IBossSpawner.IBossSpawnerBuilder spawnerBuilder = context.getAssociation(IBossSpawner.IBossSpawnerBuilder.class).get();
+
+            spawnerBuilder.overlayText(RemoRaids.getMessageConfig().get(MessageConfig.OVERLAY_TEXT));
+        } else {
+            System.out.println("no builder found :(");
+        }
+    }
+}
